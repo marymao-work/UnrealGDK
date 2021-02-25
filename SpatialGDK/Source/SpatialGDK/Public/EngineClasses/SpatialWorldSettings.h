71c7dfdf5bf9810fea47e9a45698dcec742875d9
Miron Zelina <mironzelina@improbable.io>
2021-02-25 12:00:50 +0000

    Park more

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialWorldSettings.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialWorldSettings.h
@@ -62,7 +62,12 @@ public:
 
 	// Stores the raw byte data for the snapshot, not meant to be viewed or changed manually
 	UPROPERTY(VisibleAnywhere, AdvancedDisplay, Category = "Internal SpatialOS Data")
-	TArray<uint8> RawSnapshotData;
+	TArray<uint8> RawPersistentLevelSnapshotData;
+
+	// Stores the raw byte data for the "composed" snapshot, not meant to be viewed or changed manually
+	// This would include the merged snapshot from the individual sublevels
+	UPROPERTY(VisibleAnywhere, AdvancedDisplay, Category = "Internal SpatialOS Data")
+	TArray<uint8> RawComposedSnapshotData;
 
 #if WITH_EDITORONLY_DATA
 	/** Defines how Unreal Editor will run the Tests in this map, without changing current Settings. */
