aa5b8c1fd2fe0dd2594126eb533777a9dee66fd2
Dmitrii Kozlov <dmitriikozlov@improbable.io>
2021-02-26 18:10:46 +0000

    UNR-4848: Moved PlayerSpawn-related commands to SpatialPlayerSpawner

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -2020,6 +2020,11 @@ void USpatialNetDriver::TickDispatch(float DeltaTime)
 				ClientConnectionManager->Advance();
 			}
 
+			if (IsValid(PlayerSpawner))
+			{
+				PlayerSpawner->Advance(Connection->GetCoordinator().GetViewDelta());
+			}
+
 			if (ActorSystem.IsValid())
 			{
 				ActorSystem->Advance();
